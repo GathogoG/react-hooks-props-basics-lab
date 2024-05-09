3a1d1cd38038868acf4775cc3046b6f83c62bcfd
George Gathogo <gathogogeorge11@gmail.com>
2024-05-09 10:37:31 +0300

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About(props) {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      {props.bio ? <p>{props.bio}</p> :" "}
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+    </div>
+  );
+}
+
+export default About;
